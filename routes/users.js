6fc632661ab7668fd079eb111e6b862ebec8866b
Gustavo Ripoll <gdripoll@gmail.com>
2018-11-15 18:17:20 -0300

    authorize routes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('/users');
 });
+
+router.get('/login', function (req, res, next) {
+  // res.send('/users/login')
+  res.render('users/login', {
+    title: 'CS:Users',
+    subtitle:'Login'
+  });  
+})
 
 module.exports = router;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,7 +10,7 @@ class Auth {
      */
     constructor() {
         this.logHead = `{Auth:`
-        var logFunc=`${this.logHead}constructor}`
+        var logFunc = `${this.logHead}constructor}`
         console.log(logFunc)
     }
 
@@ -19,11 +19,11 @@ class Auth {
      * @param {string} path 
      */
     setup(path) {
-        var logFunc=`${this.logHead}setup}`
+        var logFunc = `${this.logHead}setup}`
         this.config_path = path
-        console.log(logFunc,`config [${this.config_path}]`)
+        console.log(logFunc, `config [${this.config_path}]`)
 
-        this.config=JSON.parse(fs.readFileSync(`${path}/config.json`,'utf8'))
+        this.config = JSON.parse(fs.readFileSync(`${path}/config.json`, 'utf8'))
 
         // console.log(logFunc,this.config)
         return this
@@ -36,13 +36,95 @@ class Auth {
      * @param {*} next 
      */
     go(req, res, next) {
-        console.log(this)
-        var logFunc=`# >----- {Auth:go}`
-        console.log(logFunc,`url [${req.originalUrl}]`);
-        next();
+        var logFunc = `${this.logHead}go}`
+        console.log(logFunc, `url [${req.originalUrl}]`);
+        if (this.checkUrl(req.originalUrl)) {
+            next()
+        } else {
+            res.send('FALLO! falta login')
+        }
         console.log('# <-----');
     }
 
+    checkUrl(url) {
+        try {
+            var logFunc = `${this.logHead}checkUrl}`
+            // CHECK TOKEN
+            var data = this.config['auth'];
+            var segments = url.split('/')
+            segments.shift() // no el primero
+            //
+            // ACCESS Check
+            console.log(logFunc, `url=[${url}] segments=[${segments}]`)
+            var segment_tokens = this.buildBubbleTokens(segments);
+            console.log(logFunc, 'segments =', segment_tokens)
+            if (data['default-access'] == 'allow') {
+                console.log(logFunc, `[allow]`)
+                if (!data['deny'].includes('none')) {
+                    return !this.anyRouteInRoutes(segment_tokens, data['deny']) // si esta prohibido
+                }
+                return true // else, admito todo
+            } else if (data['default-access'] == 'deny') {
+                console.log(logFunc, `[deny]`)
+                return this.anyRouteInRoutes(segment_tokens, data['allow']) // si no esta permitido
+            } else {
+                var err = new Exception("Internal missconfiguration -- Please report to admin.");
+                err.outputJson();
+                die;
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    /**
+     * Builds tokens taking as a base the parameter info
+     *
+     * @param array $segments
+     * @return array contains the tokens
+     */
+    buildBubbleTokens(segments) {
+        var logFunc = `${this.logHead}buildBubbleTokens}`
+        var res = [];
+        var last = segments.length - 1;
+        for (var i = 0; i < segments.length; i++) {
+            var segment = segments.slice(0, i + 1).join('.');
+            // console.log(i, last, segment)
+            if (segment) {
+                if (i != last) {
+                    res.push(`${segment}.*`);
+                } else {
+                    res.push(segment)
+                }
+            }
+        }
+        console.log(logFunc, 'segments =', res)
+        return res;
+    }
+
+    /**
+     * Searches routes into the defined list of routes
+     *
+     * @param array $proposed routes to search for
+     * @param array $routes list of defined routes
+     * @return boolean true if at least one proposed route is found into the defined list of routes, false otherwise
+     */
+    anyRouteInRoutes(proposed, routes) {
+        var logFunc = `${this.logHead}anyRouteInRoutes}`
+        console.log(logFunc, `proposed=[${proposed}] routes=[${routes}]`)
+        for (var i in proposed) {
+            var prop = proposed[i]
+            console.log(logFunc, `check=[${prop}]`)
+            if (routes.includes(prop)) {
+                console.log(logFunc, `ok=[${prop}]`)
+                return true;
+            }
+        }
+        // Logger::debug("MWARE|Authenticate:anyRouteInRoutes not found", [implode(" | ", $proposed), implode(" | ", $routes)]);
+        return false;
+    }
+
 }
+
 
 module.exports = new Auth()
